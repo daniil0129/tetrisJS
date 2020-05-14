555a23587d3ad6f81140af05f16a14197c0ad576
Danil Shchepkov <daniil.s.h.e.p.k.o.v@gmail.com>
2020-05-14 16:05:13 +0300

    add methods to class Game: rotateBlock, rotatePiece

--- src/game.js
+++ src/game.js
@@ -30,9 +30,10 @@ export default class Game {
         blocks: [
             [0,1,0],
             [1,1,1],
-            [0,0,0]
-        ]
+            [0,0,0],
+        ],
     };
+
     movePieceLeft() {
         this.activePiece.x -= 1;
 
@@ -81,5 +82,37 @@ export default class Game {
             }
         }
 
+    }
+
+    rotatePoece() {
+        this.rotateBlocks();
+
+        if (this.hasCollision()) {
+            this.rotateBlocks(false);
+        }
+    }
+    rotateBlocks(clockwise = true) {
+        const blocks = this.activePiece.blocks;
+        const length = blocks.length;
+        const x = Math.floor(length / 2);
+        const y = length - 1;
+
+        for (let i = 0; i < x; i++) {
+            for (let j = i; j < y - i; j++) {
+                const temp = blocks[i][j];
+
+                if (clockwise) {
+                    blocks[i][j] = blocks[y - j][i];
+                    blocks[y - j][i] = blocks[y - i][y - j];
+                    blocks[y - i][y - j] = blocks[j][y - i];
+                    blocks[j][y - i] = temp;
+                } else {
+                    blocks[i][j] = blocks[j][y - i];
+                    blocks[j][y - i] = blocks[y - i][y - j];
+                    blocks[y - i][y - j] = blocks[y - j][i];
+                    blocks[y - j][i] = temp;
+                }
+            }
+        }
     }
 }
